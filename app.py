7d69ec961c6b72bde73d19feba8dbc3818623ef2
IvanShvedov <shvedov540@gmail.com>
2021-01-19 18:42:18 +0300

    a

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/polina")
+def polina():
+        return render_template('polina/index.html')
+
+
+@app.route("/dasha")
+def dasha():
+    return render_template('dasha/index.html')
+
+
+# app.run(host='0.0.0.0', port=80)
+app.run(debug=True)
+
