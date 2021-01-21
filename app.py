8b89d5ae8c63f473392030b8e6b9891096bf0036
IvanShvedov <shvedov540@gmail.com>
2021-01-21 15:45:44 +0300

    a

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ def dasha():
     return render_template('dasha/index.html')
 
 
+@app.route("/dasha/success")
+def success():
+    return render_template('dasha/index1.html')
+
+
 # app.run(host='0.0.0.0', port=80)
 app.run(debug=True)
 
